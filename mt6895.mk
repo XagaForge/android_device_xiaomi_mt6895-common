b58844b00c38dd03a27bf17bc6abf2480e4ec5c9
ChrisCatto <chriscatto@proton.me>
2025-06-22 23:17:53 +0300

    mt6895-common: Switch to common Mediatek IMS
    * Drop proprietary IMS blobs, ImsService.apk, duplicate overlays,
      props, permissions and patches.
    
    * Inherit the common Mediatek IMS makefile.
    
    Change-Id: Icfc413fd79baa635b9b5414b055bc71008bac823
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- mt6895.mk
+++ mt6895.mk
@@ -134,8 +134,7 @@ PRODUCT_PACKAGES += \
     IFAAService
 
 # IMS
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
+$(call inherit-product, vendor/mediatek/ims/ims.mk)
 
 # Light
 PRODUCT_PACKAGES += \
@@ -163,7 +162,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     FrameworksResOverlayMT6895 \
     PowerOffAlarmOverlayMT6895 \
-    TelephonyOverlayMT6895 \
     SettingsOverlayMT6895 \
     SettingsProviderResOverlayMT6895 \
     SystemUIOverlayMT6895 \
