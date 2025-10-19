d3498420169d40a03cfc5405c903083a60ad6a9c
ChrisCatto <chriscatto@proton.me>
2025-10-19 11:47:02 +0300

    fixup! mt6895-common: Refactor keymint service patching
    Change-Id: I36f69654ab45b089fd60ec599e96c857d1d1a7a1
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- mt6895.mk
+++ mt6895.mk
@@ -268,7 +268,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.hardware_keystore.xml
+    android.hardware.hardware_keystore_V3.xml
 
 # Power
 PRODUCT_PACKAGES += \
