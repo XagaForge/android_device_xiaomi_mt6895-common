5d50c381070b66f1c3822f77db3e50062729878d
Mashopy <eliasgheeraert@gmail.com>
2025-11-08 18:19:55 +0100

    mt6895-common: Build MediaTek WiFi lib
    Change-Id: I7d0dffe5bb93ed4b580a86ba021f1f9a483dd3eb
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- mt6895.mk
+++ mt6895.mk
@@ -314,6 +314,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/mediatek \
     hardware/mediatek/libaedv \
     hardware/mediatek/libmtkperf_client \
+    hardware/mediatek/wlan/wifi_hal \
     hardware/xiaomi
 
 # Speed profile services and wifi-service to reduce RAM and storage.
@@ -346,7 +347,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     wpa_supplicant \
     hostapd \
-    libwifi-hal-wrapper \
     android.hardware.wifi-service
 
 PRODUCT_PACKAGES += \
